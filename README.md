8e79f5bfec3c8137b130721940e7086d8e778931
plowzzer <poloo.p3@gmail.com>
2023-04-24 20:58:12 -0300

    install tailwind and configuring

--- README.md
+++
@@ -1,3 +0,0 @@
-# smartcambio
-
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-myfa6b)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
+  content: ["./src/**/*.{html,js}"],
   theme: {
     fontfamily: {
       body: ['InriaSans'],
     },
   },
+  plugins: [],
 };
